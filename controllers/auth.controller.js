5f6fe0db493f9425bd4588aeddd28da977898b2a
Marcos <marcos_maidana@live.com>
2023-09-13 17:07:14 -0300

    helpers & google signin

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,6 +2,7 @@ import crypto from 'crypto'
 import bcryptjs from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 import User from '../models/User.js';
+import { verify } from '../helpers/google-verify.js';
 
 const controller = {
     signup: async (req,res,next) => {
@@ -63,6 +64,62 @@ const controller = {
             })
         }
     },
+
+    googleSignin: async (req,res,next) => {
+
+        const { token_id } = req.body;
+
+        try {
+            const {name, email, photo} = await verify(token_id);
+
+            let user = await User.findOne({email}, );
+
+            if(!user) {
+                const data = {
+                    name,
+                    email,
+                    photo,
+                    password: bcryptjs.hashSync(process.env.STANDARD_PASS, 10),
+                    google: true,
+                    verify_code: crypto.randomBytes(10).toString('hex')
+                }
+                user = await User.create(data)
+            }
+            user.online=true;
+            await user.save()
+
+            const token = jwt.sign(
+                {
+                    id: user._id,
+                    email: user.email,
+                    name: user.name,
+                    photo: user.photo
+                },
+                process.env.SECRET,
+                { expiresIn: '10h'}
+            )
+            
+            res.status(200).json({
+                success: true,
+                message: 'Google user correctly logged',
+                response: {
+                    token,
+                    user: {
+                        name: user.name,
+                        email: user.email,
+                        photo: user.photo
+                    }
+                }
+            })
+
+        } catch (error) {
+            res.status(500).json({
+                success: false,
+                message:'error to authenticate user'
+            })
+        }
+    },
+
     signout: async (req,res,next) => {
         try {
             const user = await User.findOneAndUpdate(
