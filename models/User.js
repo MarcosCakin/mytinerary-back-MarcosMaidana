f4c2d8dcdba21cc052be2e35555475831ac5029c
Marcos <marcos_maidana@live.com>
2023-09-13 19:20:14 -0300

    user model updated

--- models/User.js
+++ models/User.js
@@ -6,6 +6,7 @@ const schema = new Schema({
     name : {type: String, required:true},
     email: {type: String, required:true},
     password: {type: String, required:true},
+    country : {type: String, required:true},
     photo : {type: String},
     google: {type: Boolean, default: false},
     online: {type: Boolean, default: false},
